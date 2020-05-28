4954a42a013e9262d5104847e4bea7066bf2928c
Sameer Kolhar <kolhar730@gmail.com>
2020-05-28 18:53:58 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# my-emacs-dot-d
+
+This emacs config is largely inspired by https://github.com/suvratapte/dot-emacs-dot-d
+
+Suggestions are welcome. This will be changing rapidly over time.
---
+++ init.el
@@ -0,0 +1,527 @@
+;;; package --- init.el
+;;; Commentary: This is my emacs config
+
+;; Sam's Emacs Config
+
+
+;; ─────────────────────────────────── Set up 'package' ───────────────────────────────────
+(require 'package)
+
+;; Add melpa to package archives.
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; Load and activate emacs packages. Do this first so that the packages are loaded before
+;; you start trying to modify them.  This also sets the load path.
+(package-initialize)
+
+;; Install 'use-package' if it is not installed.
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+;; ───────────────────────────────── Use better defaults ────────────────────────────────
+(setq-default
+ ;; Don't use the compiled code if its the older package.
+ load-prefer-newer t
+
+ ;; Do not show the startup message.
+ inhibit-startup-message t
+
+ ;; Do not put 'customize' config in init.el; give it another file.
+ custom-file "~/.emacs.d/custom-file.el"
+
+ ;; 72 is too less for the fontsize that I use.
+ fill-column 90
+
+ frame-title-format "Sam's Emacs"
+
+ ;; Do not create lockfiles.
+ create-lockfiles nil
+
+ ;; Don't use hard tabs
+ indent-tabs-mode nil
+
+ ;; Emacs can automatically create backup files. This tells Emacs to put all backups in
+ ;; ~/.emacs.d/backups. More info:
+ ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Backup-Files.html
+ backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+
+ ;; Allow commands to be run on minibuffers.
+ enable-recursive-minibuffers t)
+
+;; Change all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Make the command key behave as 'meta'
+(when (eq system-type 'darwin)
+  (setq mac-command-modifier 'meta)
+  (setq mac-right-command-modifier 'hyper))
+
+;; `C-x o' is a 2 step key binding. `M-o' is much easier.
+(global-set-key (kbd "M-o") 'other-window)
+
+;; Unbind `save-buffers-kill-terminal` to avoid accidentally quiting Emacs.
+(global-unset-key (kbd "C-x C-c"))
+
+;; Delete whitespace just when a file is saved.
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Enable narrowing commands.
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
+
+(global-set-key (kbd "H-d") 'narrow-to-defun)
+(global-set-key (kbd "H-t") 'narrow-to-defun)
+(global-set-key (kbd "H-w") 'widen)
+
+;; Display column number in mode line.
+(column-number-mode t)
+
+;; Automatically update buffers if file content on the disk has changed.
+(global-auto-revert-mode t)
+
+
+;; ─────────────────────────── Disable unnecessary UI elements ──────────────────────────
+(progn
+
+  ;; Do not show tool bar.
+  (when (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+
+  ;; Do not show scroll bar.
+  (when (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
+  ;; Highlight line on point.
+  (global-hl-line-mode t))
+
+
+;; ───────────────────────── Better interaction with X clipboard ────────────────────────
+(setq-default
+ ;; Makes killing/yanking interact with the clipboard.
+ x-select-enable-clipboard t
+
+ ;; To understand why this is done, read `X11 Copy & Paste to/from Emacs' section here:
+ ;; https://www.emacswiki.org/emacs/CopyAndPaste.
+ x-select-enable-primary t
+
+ ;; Save clipboard strings into kill ring before replacing them. When
+ ;; one selects something in another program to paste it into Emacs, but
+ ;; kills something in Emacs before actually pasting it, this selection
+ ;; is gone unless this variable is non-nil.
+ save-interprogram-paste-before-kill t
+
+ ;; Shows all options when running apropos. For more info,
+ ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Apropos.html.
+ apropos-do-all t
+
+ ;; Mouse yank commands yank at point instead of at click.
+ mouse-yank-at-point t)
+
+
+;; ──────────────────────── Added functionality (Generic usecases) ────────────────────────
+(defun toggle-comment-on-line ()
+  "Comment or uncomment current line."
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+
+(global-set-key (kbd "C-;") 'toggle-comment-on-line)
+
+;; Start Emacsserver so that emacsclient can be used
+(server-start)
+
+
+;; ───────────────────── Additional packages and their configurations ─────────────────────
+(require 'use-package)
+
+;; Add `:doc' support for use-package so that we can use it like what a doc-strings is for
+;; functions.
+(eval-and-compile
+  (add-to-list 'use-package-keywords :doc t)
+  (defun use-package-handler/:doc (name-symbol _keyword _docstring rest state)
+    "An identity handler for :doc.
+     Currently, the value for this keyword is being ignored.
+     This is done just to pass the compilation when :doc is included
+
+     Argument NAME-SYMBOL is the first argument to `use-package' in a declaration.
+     Argument KEYWORD here is simply :doc.
+     Argument DOCSTRING is the value supplied for :doc keyword.
+     Argument REST is the list of rest of the  keywords.
+     Argument STATE is maintained by `use-package' as it processes symbols."
+
+    ;; just process the next keywords
+    (use-package-process-keywords name-symbol rest state)))
+
+
+;; ─────────────────────────────────── Generic packages ───────────────────────────────────
+(use-package delight
+  :ensure t
+  :delight)
+
+(use-package super-save
+  :ensure t
+  :config
+  (super-save-mode +1))
+
+(use-package uniquify
+  :doc "Naming convention for files with same names"
+  :config
+  (setq uniquify-buffer-name-style 'forward)
+  :delight)
+
+(use-package recentf
+  :doc "Recent buffers in a new Emacs session"
+  :config
+  (setq recentf-auto-cleanup 'never
+        recentf-max-saved-items 1000
+        recentf-save-file (concat user-emacs-directory ".recentf"))
+  (recentf-mode t)
+  :delight)
+
+(use-package ibuffer
+  :doc "Better buffer management"
+  :bind ("C-x C-b" . ibuffer)
+  :delight)
+
+(use-package projectile
+  :doc "Project navigation"
+  :ensure t
+  :config
+  ;; Use it everywhere
+  (projectile-mode t)
+  :bind ("C-x f" . projectile-find-file)
+  :delight)
+
+(use-package magit
+  :doc "Git integration for Emacs"
+  :ensure t
+  :bind ("C-x g" . magit-status)
+  :delight)
+
+(use-package ace-jump-mode
+  :doc "Jump around the visible buffer using 'Head Chars'"
+  :ensure t
+  :bind ("C-." . ace-jump-mode)
+  :delight)
+
+(use-package dumb-jump
+  :doc "Dumb ag version of M-."
+  :ensure t
+  :bind ("C-M-." . dumb-jump-go)
+  :delight)
+
+(use-package which-key
+  :doc "Prompt the next possible key bindings after a short wait"
+  :ensure t
+  :config
+  (which-key-mode t)
+  :delight)
+
+(use-package ivy
+  :doc "A generic completion mechanism"
+  :ensure t
+  :config
+  (ivy-mode t)
+  (setq ivy-use-virtual-buffers t
+        ;; Display index and count both.
+        ivy-count-format "(%d/%d)"
+        ;; By default, all ivy prompts start with `^'. Disable that.
+        ivy-initial-inputs-alist nil)
+
+  :bind (("C-x b" . ivy-switch-buffer)
+         ("C-x B" . ivy-switch-buffer-other-window))
+  :delight)
+
+(use-package ivy-rich
+  :doc "Have additional information in empty space of ivy buffers."
+  :disabled t
+  :ensure t
+  :custom
+  (ivy-rich-path-style 'abbreviate)
+  :config
+  (setcdr (assq t ivy-format-functions-alist)
+          #'ivy-format-function-line)
+  (ivy-rich-mode 1)
+  :delight)
+
+(use-package ivy-posframe
+  :doc "Custom positions for ivy buffers."
+  :ensure t
+  :config
+  (setq ivy-posframe-parameters
+        '((font . "Fira Code Retina 14")))
+  (ivy-posframe-mode 1)
+  :delight)
+
+(use-package swiper
+  :doc "A better search"
+  :ensure t
+  :bind (("C-s" . swiper-isearch)
+         ("H-s" . isearch-forward-regexp))
+  :delight)
+
+(use-package counsel
+  :doc "Ivy enhanced Emacs commands"
+  :ensure t
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-'" . counsel-imenu)
+         ("C-c s" . counsel-rg)
+         :map counsel-find-file-map
+         ("RET" . ivy-alt-done))
+  :delight)
+
+(use-package aggressive-indent
+  :doc "Intended Indentation"
+  :ensure t
+  :config
+  (add-hook 'before-save-hook 'aggressive-indent-indent-defun)
+  :delight)
+
+(use-package diminish
+  :doc "Hide minor modes from mode line"
+  :ensure t
+  :delight)
+
+(use-package flyspell
+  :config
+
+  ;; Flyspell should be able to learn a word without the
+  ;; `flyspell-correct-word-before-point` pop up.
+  ;; Refer:
+  ;; https://stackoverflow.com/questions/22107182/in-emacs-flyspell-mode-how-to-add-new-word-to-dictionary
+  (defun flyspell-learn-word-at-point ()
+    "Add word at point to list of correct words."
+    (interactive)
+    (let ((current-location (point))
+          (word (flyspell-get-word)))
+      (when (consp word)
+        (flyspell-do-correct 'save nil
+                             (car word) current-location
+                             (cadr word) (caddr word)
+                             current-location))))
+
+  ;; This color is specific to `nord` theme.
+  (set-face-attribute 'flyspell-incorrect nil :underline '(:style line :color "#bf616a"))
+  (set-face-attribute 'flyspell-duplicate nil :underline '(:style line :color "#bf616a"))
+
+  :bind ("H-l" . flyspell-learn-word-at-point))
+
+
+;; ───────────────────────────────────── Code editing ─────────────────────────────────────
+
+(use-package company
+  :doc "COMplete ANYthing"
+  :ensure t
+  :bind (:map
+         global-map
+         ("TAB" . company-complete-common-or-cycle)
+         ;; Use hippie expand as secondary auto complete. It is useful as it is
+         ;; 'buffer-content' aware (it uses all buffers for that).
+         ("M-/" . hippie-expand)
+         :map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous))
+  :config
+  (setq company-idle-delay 0.1)
+  (global-company-mode t)
+
+  ;; Configure hippie expand as well.
+  (setq hippie-expand-try-functions-list
+        '(try-expand-dabbrev
+          try-expand-dabbrev-all-buffers
+          try-expand-dabbrev-from-kill
+          try-complete-lisp-symbol-partially
+          try-complete-lisp-symbol))
+  :delight)
+
+(use-package paredit
+  :doc "Better handling of paranthesis when writing Lisp"
+  :ensure t
+  :init
+  (add-hook 'clojure-mode-hook #'enable-paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'enable-paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook #'enable-paredit-mode)
+  :config
+  (show-paren-mode t)
+  :bind (("M-[" . paredit-wrap-square)
+         ("M-{" . paredit-wrap-curly))
+  :delight)
+
+(use-package rainbow-delimiters
+  :doc "Colorful paranthesis matching"
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+  :delight)
+
+(use-package highlight-symbol
+  :doc "Highlight and jump to symbols"
+  :ensure t
+  :config
+  (set-face-background 'highlight-symbol-face (face-background 'highlight))
+  (setq highlight-symbol-idle-delay 0.5)
+  (add-hook 'prog-mode-hook 'highlight-symbol-mode)
+  :bind (("M-n" . highlight-symbol-next)
+         ("M-p" . highlight-symbol-prev))
+  :delight)
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode t)
+  (add-to-list 'hippie-expand-try-functions-list
+               'yas-hippie-try-expand)
+  :delight)
+
+
+;; ──────────────────────────────── Programming languages ───────────────────────────────
+(use-package clojure-mode
+  :doc "A major mode for editing Clojure code"
+  :ensure t
+  :config
+  ;; This is useful for working with camel-case tokens, like names of
+  ;; Java classes (e.g. JavaClassName)
+  (add-hook 'clojure-mode-hook #'subword-mode)
+  :delight)
+
+(use-package cider
+  :doc "Integration with a Clojure REPL cider"
+  :ensure t
+
+  :init
+  ;; Enable minibuffer documentation
+  (add-hook 'cider-mode-hook 'eldoc-mode)
+
+  :config
+  ;; Go right to the REPL buffer when it's finished connecting
+  (setq cider-repl-pop-to-buffer-on-connect t)
+
+  ;; When there's a cider error, show its buffer and switch to it
+  (setq cider-show-error-buffer t)
+  (setq cider-auto-select-error-buffer t)
+
+  ;; Where to store the cider history.
+  (setq cider-repl-history-file "~/.emacs.d/cider-history")
+
+  ;; Wrap when navigating history.
+  (setq cider-repl-wrap-history t)
+
+  ;; Attempt to jump at the symbol under the point without having to press RET
+  (setq cider-prompt-for-symbol nil)
+
+  ;; Always pretty print
+  (setq cider-repl-use-pretty-printing t)
+
+  ;; Log client-server messaging in *nrepl-messages* buffer
+  (setq nrepl-log-messages nil)
+
+  ;; REPL should expect input on the next line + unnecessary palm trees!
+  (defun cider-repl-prompt-custom (namespace)
+    "Return a prompt string that mentions NAMESPACE."
+    (format " %s \n" namespace))
+
+  (setq cider-repl-prompt-function 'cider-repl-prompt-custom)
+
+  :bind (:map
+         cider-mode-map
+         ("H-t" . cider-test-run-test)
+         ("H-n" . cider-test-run-ns-tests)
+         :map
+         cider-repl-mode-map
+         ("C-c M-o" . cider-repl-clear-buffer))
+  :delight)
+
+(use-package flycheck
+  :ensure t
+  :config
+  (global-flycheck-mode)
+
+  ;; Do not display errors on left fringe.
+  (setq flycheck-indication-mode nil)
+  :delight)
+
+(use-package flycheck-joker
+  :after clojure-mode
+  :ensure t
+  :delight)
+
+(use-package flycheck-clj-kondo
+  :ensure t
+  :after clojure-mode
+  :config
+  (dolist (checkers '((clj-kondo-clj . clojure-joker)
+                      (clj-kondo-cljs . clojurescript-joker)
+                      (clj-kondo-cljc . clojure-joker)
+                      (clj-kondo-edn . edn-joker)))
+    (flycheck-add-next-checker (car checkers) (cons 'error (cdr checkers))))
+  :delight)
+
+(use-package eldoc
+  :doc "Easily accessible documentation for Elisp"
+  :config
+  (global-eldoc-mode t)
+  :delight)
+
+
+;; ──────────────────────────────────── Look and feel ──────────────────────────────────
+
+(use-package powerline
+  :doc "Better mode line"
+  :ensure t
+  :config
+  (powerline-center-theme)
+  :delight)
+
+(use-package gruvbox-theme
+  :ensure t
+  :disabled t
+  :config
+  (load-theme 'gruvbox t))
+
+(use-package kaolin-themes
+  :ensure t
+  :disabled t
+  :config
+  (load-theme 'kaolin-dark t))
+
+(use-package sublime-themes
+  :ensure t
+  :config
+  (load-theme 'spolsky t))
+
+
+;; ──────────────────────────────────────── Keybindings and Misc. ───────────────────────────────────────
+
+;; this package also requires that we download the indium server `npm i -g indium`
+(use-package indium
+  :ensure t
+  :doc "The complete JS mode for Emacs")
+
+(global-set-key
+ (kbd "M-<return>") 'other-window)
+
+(global-set-key
+ (kbd "M-<left>") 'previous-window)
+
+(global-set-key
+ (kbd "C-f") 'find-function)
+
+(global-set-key
+ (kbd "C-v") 'find-variable)
+
+(set-frame-font "IBM Plex Mono 15")
+
+(windmove-default-keybindings)
+(delete-other-windows)
+
+(provide 'init)
+
+;;; init.el ends here
